c63f662a2487722a36f3d9a381fdc3a47e3e71a7
ajipaon <ajisetiawan883@gmail.com>
2024-09-27 10:28:03 +0700

    dafa

---
+++ src/app/api/register/route.ts
@@ -0,0 +1,72 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function POST(req: NextRequest) {
+    try {
+        const { username, password } = await req.json();
+
+        // Validasi input
+        if (!username || !password) {
+            return NextResponse.json({ error: 'Username and password are required' }, { status: 400 });
+        }
+
+        const api_key = process.env.NEXT_TOKEN_EMBY as string || '08e470d0535045818292772381e2e029';
+        const user_template = process.env.NEXT_USER_TEMPLATE_FREE_EMBY as string;
+        console.log(user_template)
+
+    const response = await fetch(`http://localhost:8096/emby/Users/New?api_key=${api_key}`, {
+        method: 'POST',
+        headers: {
+            'accept': 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            Name: username,
+            CopyFromUserId: user_template,
+            UserCopyOptions: [
+              "UserPolicy",
+              "UserConfiguration"
+            ]
+        }),
+    });
+
+    const result = await response.json();
+    if(result.Id) {
+         await fetch(`http://localhost:8096/emby/Users/${result.Id}/Password?api_key=${api_key}` , {
+            method: 'POST',
+            headers: {
+                'accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                Id: result.Id,
+                NewPw: password,
+                ResetPassword: false
+            }),
+        });
+    
+    }else{
+        return NextResponse.json({ error: 'gagal membuat user' }, { status: 400 });
+    }
+
+    if (!response.ok) {
+        return NextResponse.json({ error: 'Failed to register user' }, { status: response.status });
+    }
+        return NextResponse.json({ message: 'User registered successfully' }, { status: 200 });
+    } catch (error) {
+        // Tangani kesalahan parsing JSON atau lainnya
+        return NextResponse.json({ error: 'Invalid request' }, { status: 400 });
+    }
+}
+
+export function OPTIONS() {
+    const headers = {
+        'Access-Control-Allow-Origin': '*', 
+        'Access-Control-Allow-Methods': 'POST, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+    };
+
+    return new NextResponse(null, {
+        status: 204, // No Content, untuk OPTIONS
+        headers,
+    });
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,24 +2,35 @@
 import { useRouter } from 'next/navigation';
 import React, { useState } from 'react';
 import 'tailwindcss/tailwind.css';
+import 'react-toastify/dist/ReactToastify.css';
 
+import { ToastContainer, toast } from 'react-toastify';
 const RegisterPage: React.FC = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-
+  const [isLoading, setIsLoading] = useState(false);
   const router = useRouter();
 
+  const success_login = (kata: string) => toast(kata);
+  
   const handleLink = () => {
-    // Deteksi perangkat pengguna
+
+    setIsLoading(true);
     const userAgent = navigator.userAgent || (window as unknown as { opera?: string }).opera || '';
 
-    const deepLink = 'emby://'; // Deep link untuk membuka aplikasi Emby
+    // const deepLink = 'intent://home#Intent;package=com.mb.android;scheme=emby;end';
+    // const deepLink = 'intent://home#Intent;scheme=com.mb.android;package=com.mb.android;end';
+    const deepLink = 'emby://192.168.0.160:8096';
 
+    
     console.log(userAgent)
     if (/android/i.test(userAgent)) {
 
       window.location.href = deepLink;
       console.log("android")
+      setTimeout(() => {
+        window.location.href = 'http://192.168.0.160:8096';
+      }, 3000); // 3 detik
 
       // setTimeout(() => {
       //   window.location.href = 'https://play.google.com/store/apps/details?id=com.emby.emby.mobile';
@@ -34,19 +45,72 @@ const RegisterPage: React.FC = () => {
     } else {
       console.log("browser")
 
-      router.push('http://localhost:8096');
+      router.push(process.env.NEXT_PUBLIC__API_EMBY || 'http://localhost:8096');
     }
   }
 
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    console.log({ username, password });
+    if(!username || !password) {
+      alert('Username and password harus diisi');
+      return;
+    }
+    setIsLoading(true);
+    fetch('/api/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username, password }),
+    })
+      .then(async (res) => {
+        if (res.ok) {
+          const data = await res.json();
+          success_login(data.message)
+          setUsername('');
+          setPassword('');
+        } else {
+          success_login('Failed to register user');
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        alert('Failed to register user');
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+
   };
 
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-r from-slate-600 via-zinc-400 to-stone-600 p-1">
-      <div className="bg-red-500 text-white text-center p-4 rounded-lg mb-4 w-full max-w-md flex flex-col items-center">
+    <ToastContainer />
+      {isLoading && (
+        <div style={{
+          position: 'fixed',
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '100%',
+          backgroundColor: 'rgba(0, 0, 0, 0.5)',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          zIndex: 9999
+        }}>
+          <div style={{
+            width: '100px',
+            height: '100px',
+            border: '16px solid #f3f3f3',
+            borderRadius: '50%',
+            borderTop: '16px solid #3498db',
+            animation: 'spin 2s linear infinite'
+          }}></div>
+        </div>
+      )}
+      <div className="bg-red-500 text-white text-center p-1 rounded-lg mb-2 w-full max-w-md flex flex-col items-center">
         <div className="flex items-center mb-2">
           <svg className="w-6 h-6 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 8v4m0 4h.01M12 2a10 10 0 100 20 10 10 0 000-20z"></path>
@@ -56,12 +120,31 @@ const RegisterPage: React.FC = () => {
         <hr className="w-full border-gray-300 mb-2"/>
         <span className="font-bold text-md">wifi ini hanya menyediakan film offline yang dapat diakses oleh pengguna yang terhubung ke wifi ini.</span>
       </div>
-      <div className="bg-orange-400 p-3 rounded-lg shadow-lg w-full max-w-md">
+      <div className="bg-orange-400 p-1 rounded-lg shadow-lg w-full max-w-md">
         <h2 className="text-2xl font-bold mb-1 text-center">Register</h2>
         <div className="bg-orange-300 text-gray-950 text-center p-4 rounded-lg mb-4 w-full max-w-md flex flex-col items-center">
         <div className="flex items-center mb-2">
             <span className="font-extrabold text-lg">
-            Jika sudah memiliki akun, kamu bisa langsung menuju halaman <button className="text-blue-700 hover:text-blue-900 underline" onClick={handleLink}>DISINI</button>
+            Jika sudah memiliki akun, kamu bisa langsung menuju halaman <button className="text-blue-700 hover:text-blue-900 underline" onClick={handleLink}>DISINI</button> atau buka dengan apk Emby di perangkat kamu. dan pastekan link ini<span className="font-bold text-cyan-700">
+            <button
+                className="text-cyan-700 underline"
+                onClick={() => {
+                if (navigator.clipboard) {
+                  navigator.clipboard.writeText(process.env.NEXT_PUBLIC__API_EMBY || 'http://localhost:8096')
+                  .then(() => {
+                    alert('Link copied to clipboard!');
+                  })
+                  .catch((err: any) => {
+                    console.error('Could not copy text: ', err);
+                  });
+                } else {
+                  console.error('Clipboard API not supported');
+                }
+                }}
+              >
+              {process.env.NEXT_PUBLIC__API_EMBY || 'http://localhost:8096'}
+              </button>
+            </span>
             </span>
         </div>
       </div>
@@ -81,7 +164,8 @@ const RegisterPage: React.FC = () => {
           </div>
           <div className="mb-6">
             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
-              Password
+            Password  
+              <span className="text-red-500">* pin default kamu adalah 1234</span>
             </label>
             <input
               type="password"
@@ -92,7 +176,7 @@ const RegisterPage: React.FC = () => {
               required
             />
           </div>
-          <div className="flex items-center justify-between">
+          <div className="flex items-center justify-center">
             <button
               type="submit"
               className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full sm:w-auto"
